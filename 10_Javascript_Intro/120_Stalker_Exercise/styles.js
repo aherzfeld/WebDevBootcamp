f93167167dba2dfd755d241aebcdc29680102ab3
aherzfeld <herzfeld.andrew@gmail.com>
2019-01-17 16:21:25 +0800

    finished JS Stalker exercise

---
+++ 10_Javascript_Intro/120_Stalker_Exercise/styles.js
@@ -0,0 +1,9 @@
+var first = prompt("What is your first name?");
+
+var last = prompt("What is your last name?");
+
+var age = prompt("What is your age?");
+
+alert("Hi " + first + " " + last);
+
+console.log("You are " + age + " years old sir.");
